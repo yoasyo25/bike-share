1e05add8aee9ef753b657841a6f4f899fea81511
Wil Donovan <wil.donovan@gmail.com>
2017-07-01 13:54:57 -0600

    Created cities table and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701194912) do
+ActiveRecord::Schema.define(version: 20170701195331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dates", force: :cascade do |t|
     t.date     "date"
