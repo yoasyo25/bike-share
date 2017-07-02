04b578d0a90844990660a053417eb9ce5451e14d
Wil Donovan <wil.donovan@gmail.com>
2017-07-01 21:52:47 -0600

    built out feature testing and new erb with select options and date input and then partially built out the error message method and routed the controller

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -4,4 +4,23 @@ class BikeShareApp < Sinatra::Base
     erb :index
   end
 
+  # get '/stations/error'
+  #   erb :error
+  # end
+
+  get '/stations/new' do
+    @station = Station.all
+    @city = City.all
+    erb :new
+  end
+
+  post '/stations' do
+    # require 'pry'; binding.pry
+    if params[:station[:name]] == nil
+      redirect "/stations/error"
+    else
+      @station = Station.create(params[:station])
+      redirect "/stations/#{@station.id}"
+    end
+  end
 end
