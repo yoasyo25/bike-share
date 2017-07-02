1fc68baf4a267e5d26f33f30a5a4664dcddd2827
Yohanan Assefa <nanahoy@gmail.com>
2017-07-02 12:37:40 -0600

    Feature test for Updating a Station

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -4,4 +4,18 @@ class BikeShareApp < Sinatra::Base
     erb :index
   end
 
+  get '/stations' do
+    erb :"stations/index"
+  end
+
+  get '/stations/:id/edit' do
+    @station = Station.find(params[:id])
+    erb :"stations/edit"
+  end
+
+  put '/stations/:id' do |id|
+    @station = Station.update(id.to_i, params[:station])
+    redirect "/stations/#{id}"
+  end
+
 end
