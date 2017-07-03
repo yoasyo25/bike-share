601210c7a93c910bd555fd93c4e45bc867059e70
Wil Donovan <wil.donovan@gmail.com>
2017-07-03 10:28:02 -0600

    time comes in as a string now

--- app/controllers/bike_share_app.rb
+++ app/controllers/bike_share_app.rb
@@ -9,6 +9,10 @@ class BikeShareApp < Sinatra::Base
     erb :"/stations/index"
   end
 
+  get '/trips' do
+    @trip
+  end
+
   get '/stations/station-dashboard' do
     erb :stations_dashboard
   end
