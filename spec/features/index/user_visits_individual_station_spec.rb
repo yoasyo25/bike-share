e126e38fe7bb33c539a9654727448a570d3952bf
Daniel Phillips <danieledward.phillips@gmail.com>
2017-07-03 10:00:30 -0600

    adds view

---
+++ spec/features/index/user_visits_individual_station_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe "User visits individual station" do
+  it "sees individual station" do
+    # As a user
+    # When I visit the first station's page
+
+    station = Station.create(name: , dock_count: , city_id: , installation_date: )
+
+    visit "/station/#{station.id}"
+
+
+    # I should see a station name , dock_count, city_id, installation_date
+    expect(current_path).to eq("/station/#{station.id}")
+    expect(page).to have_content("San Jose Diridon Caltrain Station")
+    expect(page).to have_content("27")
+    expect(page).to have_content("1")
+    expect(page).to have_content("2013-06-08")
+  end
+
+end
