bfe1920155b6976622841f9e438d0f557036b237
Katie Shermer <katieshermer@gmail.com>
2017-07-05 08:32:48 -0600

    Feature testing for deleting a weather day

---
+++ spec/features/weather/user_deletes_a_weather_day_spec.rb
@@ -0,0 +1,14 @@
+RSpec.describe "User visits the weather index page and" do
+
+  it "and clicks the delete button on a weather day" do
+    @weather_date = BikeDate.create("2017-04-19")
+    @weather_day = Weather.create(date: @weather_date.id, max_temp: 85.0, mean_temp: 83.0, min_temp: 81.0, mean_humidity: 77.0, mean_visibility: 7.0, mean_wind: 12.0, precipitation: 1.0)
+
+    visit '/weather'
+
+    click_button("Delete")
+
+    expect(current_path).to eq("/weather")
+    expect(page).to_not have_content("April 19, 2017")
+  end
+end
