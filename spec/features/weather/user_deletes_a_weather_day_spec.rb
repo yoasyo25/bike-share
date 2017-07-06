e5a613c30804c71395b01fb4a190f1809b680455
Katie Shermer <katieshermer@gmail.com>
2017-07-06 10:00:44 -0600

    Added feature testing for weather dashboard

--- spec/features/weather/user_deletes_a_weather_day_spec.rb
+++ spec/features/weather/user_deletes_a_weather_day_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe "User visits the weather index page and" do
 
   it "and clicks the delete button on a weather day" do
-    @weather_date = BikeDate.create(date: "2017-04-19")
+    @weather_date = BikeDate.find_or_create_by(date: "2017-04-19")
     @weather_day = Condition.create(date_id: @weather_date.id, max_temp: 85.0, mean_temp: 83.0, min_temp: 81.0, mean_humidity: 77.0, mean_visibility: 7.0, mean_wind_speed: 12.0, precipitation: 1.0)
 
     visit '/conditions'
